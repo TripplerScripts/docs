3b472390b7cc7fba34e6a6beced901246b1a17ef
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-10-13 20:45:30 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trippler Documentation
+
+## Acknowledgements
+
+- [`Qbox-project/qbox-project.github.io`](https://github.com/Qbox-project/qbox-project.github.io) - fork
---
+++ theme.config.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://trippler.shop${asPath}`;
+  const description = frontMatter.description || "Documentation for Trippler";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="https://avatars.githubusercontent.com/tripplerscripts" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = (arr[1][0] !== "#" && arr[1]) || "Trippler";
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? "Documentation" : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div
+      style={{
+        paddingLeft: "50px",
+        lineHeight: "38px",
+        background: "url(https://avatars.githubusercontent.com/tripplerscripts) no-repeat left",
+        backgroundSize: "38px",
+        fontWeight: 550,
+      }}
+    >
+      Trippler
+    </div>
+  ),
+  project: {
+    link: "https://github.com/TripplerScripts",
+  },
+  chat: {
+    link: "https://discord.gg/vPwaW3rpUk",
+  },
+  docsRepositoryBase: "https://github.com/tripplerscripts/docs/blob/main",
+  footer: {
+    text: "© 2025 Trippler",
+  },
+  primaryHue: { dark: 170, light: 170 },
+  head: useHead,
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;

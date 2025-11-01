146853172cadba4777f137cf75f0a39ee30066f3
Lenix <lenix01x@gmail.com>
2025-11-01 16:33:00 +0100

    Commit

--- components/icons.jsx
+++ components/icons.jsx
@@ -2,3 +2,4 @@ const iconStyle = { width: '24px', height: '24px' }
 
 export const GithubIcon = () => <img src="/github.svg" alt="GitHub" style={iconStyle} />
 export const DownloadIcon = () => <img src="/download.svg" alt="Download" style={iconStyle} />
+export const TagIcon = () => <img src="/tag.svg" alt="Get" style={iconStyle} />

d0a87d5f050552f64ae5a784ee46ffa58eadfefd
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-01-03 14:22:01 -0800

    #

--- components/icons.jsx
+++ components/icons.jsx
@@ -1,5 +1,6 @@
 const iconStyle = { width: '24px', height: '24px' }
 
+export const NPMIcon = () => <img src="/npm.svg" alt="NPM" style={iconStyle} />
 export const GithubIcon = () => <img src="/github.svg" alt="GitHub" style={iconStyle} />
 export const DownloadIcon = () => <img src="/download.svg" alt="Download" style={iconStyle} />
 export const TagIcon = () => <img src="/tag.svg" alt="Get" style={iconStyle} />
